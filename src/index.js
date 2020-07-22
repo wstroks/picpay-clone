a5b3edda4970c8a92a1550b2a309e26d0822308e
Washington Pagotto Batista <wstroks@hotmail.com>
2020-07-22 16:17:32 -0300

    interface version 2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, StatusBar } from 'react-native';
 import Navigation from './Navigation';
 import { NavigationContainer } from '@react-navigation/native';
 
 
 export default function App() {
   return (
+    <>
+    <StatusBar barStyle='light-content' backgroundColor='#000'/>
     <NavigationContainer>
       <Navigation />
 
     </NavigationContainer>
+    </>
     
   );
 }

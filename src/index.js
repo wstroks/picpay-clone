3c1fbe85d6eeb28ca9ae921370d3cc6518fc988a
Washington Pagotto Batista <wstroks@hotmail.com>
2020-07-22 11:38:01 -0300

    interface version 1

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text } from 'react-native';
+import Navigation from './Navigation';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Navigation />
+
+    </NavigationContainer>
+    
+  );
+}
+
